92eadfa7dd21bc867dd719286e86134467577b4b
dedeambari <if23.dedeambari@mhs.ubpkarawang.ac.id>
2025-05-09 11:31:36 +0700

    First Commit

--- resources/views/report/staf/pdf/content.blade.php
+++ resources/views/report/staf/pdf/content.blade.php
@@ -4,7 +4,7 @@
         <h1 style="text-align: center; background-color: #f5f5f5; padding: 10px border-radius: 5px">
             {{ $title }}
         </h1>
-        @include('report.customer.table', $data)
+        @include('report.staf.table', $data)
     </div>
     <footer class='footer'>
         <p style='text-align: center; margin-bottom: 5px'>Copyright &copy; Laundry Version Laravel</p>
